eed723d4aa78c0d952e0f5c3ff38f509d4576077
adriannicolai <anlumbad@gmail.com>
2022-08-04 13:16:18 +0800

    addedsession and body parser

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const express    = require('express');
 const app        = express();
 const port       = 3001;
 const path       = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const session    = require('express-session');
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+/* Checker if user wants to go to home page without session */
+app.use((req, res, next) => {
+    console.log(`the request url is ${req.url}`);
+    // if(req.session.user){
+    //     res.redirect("/");
+    // }
+    next();
+});
+/* json of users
+structure: {
+    "email@gmail.com": "email@email.com",
+    "password": "password",
+    "balance": "balance"
+}
+*/
+let users        = {}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -17,6 +41,10 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     res.render('signup_page');
+});
+
+app.get("*", (req, res) =>{
+    
 });
 
 
